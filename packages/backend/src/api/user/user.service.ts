bb7c885d88307beef6e407c94ae02110734bb996
Romendra Uttam <120070407+uttam-on-git@users.noreply.github.com>
2025-09-14 12:37:50 +0530

    feat(backend): implement user registration with prisma and docker

---
+++ packages/backend/src/api/user/user.service.ts
@@ -0,0 +1,24 @@
+import { User } from "@prisma/client";
+import db from "../../lib/db";
+import bcrypt from "bcryptjs"
+
+export const createUser = async (input: Pick<User, 'email' | 'password'>) => {
+    const { email, password} = input
+    //hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await db.user.create({
+        data: {
+            email: email.toLowerCase(),
+            password: hashedPassword,
+        },
+    });
+
+    return user
+}
+
+export const findUserByEmail = async (email: string) => {
+  return await db.user.findUnique({
+    where: { email: email.toLowerCase() },
+  });
+};
