aefd5df427692ad1ce410a82b2a5abb43041501d
Romendra Uttam <120070407+uttam-on-git@users.noreply.github.com>
2025-09-16 22:42:46 +0530

    fix(backend): cors error

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -12,7 +12,7 @@ const port = process.env.PORT || 8000
 //middleware
 app.use(cookieParser())
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
   credentials: true,
 }));
 app.use(express.json())
